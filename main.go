128cb168524c7ec80fb15775278f1bd69036acc5
Nik Schultz <nschultz@cranksoftware.com>
2016-02-20 16:26:35 -0500

    initial commit for the race place app

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func main() {
+	// http.HandleFunc("/", handler)
+	http.HandleFunc("/echo", echoHandler)
+	http.Handle("/", http.FileServer(http.Dir(".")))
+	http.ListenAndServe(":8080", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, welcome to Race Place %s!", r.URL.Path[1:])
+}
+
+func echoHandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		//log.Println(err)
+		return
+	}
+
+	for {
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+			return
+		}
+
+		printBinary(p)
+
+		err = conn.WriteMessage(messageType, p)
+		if err != nil {
+			return
+		}
+	}
+}
+
+func printBinary(s []byte) {
+	fmt.Printf("Received b:")
+	for n := 0; n < len(s); n++ {
+		fmt.Printf("%d,", s[n])
+	}
+	fmt.Printf("\n")
+}
